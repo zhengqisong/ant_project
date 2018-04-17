6743c7d9089a1d49db76aaa8cbb07e1626719dcc
zhengqsa <zhengqsa@digitalchina.com>
2018-04-17 14:21:14 +0800

    test

--- src/ant_project/HelloWorld.java
+++ src/ant_project/HelloWorld.java
@@ -1,8 +1,8 @@
 package ant_project;
 
 public class HelloWorld {
-	 public String sayHello(String name){ 
+	  public String sayHello(String name){ 
 		 
-		 return "Hello "+"<dsadsadsafdsafds?"+name; 
+		 return "Hello "+"<dsadsadsafdsafds?>>>"+name; 
 	 } 
 }
